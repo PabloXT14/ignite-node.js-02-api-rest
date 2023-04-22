424ea6db869528fe0795f1078e1215334a51f8f1
Pablo Alan <pabloxt14@gmail.com>
2023-04-22 18:30:01 -0300

    feat: trining querys with knex

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,26 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const PORT = 3333
 const server = fastify()
 
-server.get('/', async (request, reply) => {
-  const test = await knex('sqlite_schema').select('*')
+server.get('/transactions', async (request, reply) => {
+  const test = await knex('transactions').select('*')
 
   return test
+})
+
+server.post('/transactions', async (request, reply) => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de test',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transaction
 })
 
 server
