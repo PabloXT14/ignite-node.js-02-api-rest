916b45813ad39db1096436eaaac6c2872ae11cfd
Pablo Alan <pabloxt14@gmail.com>
2023-04-27 17:23:37 -0300

    feat: creating route post transactions and valided body with zod

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,21 +1,32 @@
 import crypto from 'node:crypto'
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/transactions', async (request, reply) => {
-    const test = await knex('transactions').select('*')
-    return test
+  app.get('/', async (request, reply) => {
+    const transactions = await knex('transactions').select('*')
+    return reply.status(200).send(transactions)
   })
 
-  app.post('/transactions', async (request, reply) => {
-    const transaction = await knex('transactions')
-      .insert({
-        id: crypto.randomUUID(),
-        title: 'Transação de test',
-        amount: 1000,
-      })
-      .returning('*')
-    return transaction
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
