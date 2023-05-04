477fa90437ce28ff443da76f0f16f83834d9d4de
Pablo Alan <pabloxt14@gmail.com>
2023-05-04 16:16:13 -0300

    feat: testing if user can list all transactions

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,23 +1,50 @@
-import { afterAll, beforeAll, it } from 'vitest'
+import { afterAll, beforeAll, describe, it, expect } from 'vitest'
 import request from 'supertest'
 
 import { app } from '../src/app'
 
-beforeAll(async () => {
-  await app.ready() // esperando o servidor estar pronto
-})
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready() // esperando o servidor estar pronto
+  })
 
-afterAll(async () => {
-  await app.close() // encerrando o servidor
-})
+  afterAll(async () => {
+    await app.close() // encerrando o servidor
+  })
 
-it('should user be able to create a new transaction', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New Transaction',
-      amount: 3000,
-      type: 'credit',
-    })
-    .expect(201)
+  it('should user be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 3000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+
+  it('should user be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 5000,
+      }),
+    ])
+  })
 })
